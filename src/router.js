cc5e58093816f09e8ef00b9fb3bfc1fdc96064df
은배 <geb9211@gmail.com>
2022-09-22 13:57:44 +0900

    새로고침시 로그인 여부, 상태 불러오기 구현

--- src/router.js
+++ src/router.js
@@ -1,15 +1,28 @@
+import { useState } from 'react';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from './firebase.config.js';
+
 import Main from "./Pages/main";
 import Auth from "./Pages/auth";
 import Order from "./Pages/order";
+import Register from "./Pages/register";
 
 const Router = () => {
+  // 새로고침시 유저 로그인 여부, 상태 불러오기
+  const [isLogin, setIsLogin] = useState(false);
+  onAuthStateChanged(auth, (user) => {
+    if(user) setIsLogin(true); 
+      else setIsLogin(false);
+  })
+
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/auth" element={<Auth />} />
-        <Route path="/order" element={<Order />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/order" element={<Order isLogin={isLogin} />} />
       </Routes>
     </BrowserRouter>
   );
