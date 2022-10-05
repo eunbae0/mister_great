08bc49894d98370dd9097f924c4d1390008196c2
BeomJo-uos <81895680+BeomJo-uos@users.noreply.github.com>
2022-10-05 16:31:56 +0900

    Initial commit

--- src/components/order/selectDinner.jsx
+++ src/components/order/selectDinner.jsx
@@ -4,8 +4,21 @@ function SelectDinner({ setProgress }) {
   const object = {dinnerStyle: '', dinnerType: '',};
   const [obj, setObj] = useState(object);
 
-  const onChangeSelect = () => {
+  const onChangeSelect = (e) => {
     // ../Pages/register.jsx 파일 참고해서 작성해주세요
+    const type = e.target.type;
+    if (type === 'menu') {
+      setAuthObj((prev) => {
+        return { ...prev, email: e.target.value};
+      });
+    }
+    else if (type === 'style') {
+      setAuthObj((prev) => {
+        return { ...prev, password: e.target.value};
+      });
+    } else {
+      console.log('error');
+    }
   };
 
   const onSubmitSelect = (e) => {
@@ -17,19 +30,20 @@ function SelectDinner({ setProgress }) {
   return (
     <div>
       <form action="submit" onSubmit={onSubmitSelect}>
-        {/* 디너 스타일 */}
-        <select onChange={onChangeSelect}>
+        {/* 디너 메뉴 */}
+        <select type = "menu" onChange={onChangeSelect}>
           <option value="">--Please choose an option--</option>
-          <option value="dog">Dog</option>
-          <option value="cat">Cat</option>
-          <option value="hamster">Hamster</option>
+          <option value="valentine">Valentine dinner</option>
+          <option value="french">French dinner</option>
+          <option value="english">English dinner</option>
+          <option value="champagne">Champagne Feast dinner</option>
         </select>
-        {/* 디너 종류 */}
-        <select onChange={onChangeSelect}>
+        {/* 디너 스타일 */}
+        <select type="style" onChange={onChangeSelect}>
           <option value="">--Please choose an option--</option>
-          <option value="dog">Dog</option>
-          <option value="cat">Cat</option>
-          <option value="hamster">Hamster</option>
+          <option value="simple">Simple</option>
+          <option value="grand">Grand</option>
+          <option value="deluex">Deluex</option>
         </select>
         <button type="submit">
           다음 단계
