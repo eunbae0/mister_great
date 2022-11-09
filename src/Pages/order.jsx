e1f6c059fef5be20d2640bd888fe85594db581b8
은배 <geb9211@gmail.com>
2022-11-09 15:43:46 +0900

    ✨ 주문목록 제거기능 및 파이어베이스 연동

--- src/Pages/order.jsx
+++ src/Pages/order.jsx
@@ -33,24 +33,47 @@ function progressChanger (progress, setProgress, orderId, setOrderId, isLogin, u
   }
 }
 
-function Order({ isLogin, uid }) {
-  const [progress, setProgress] = useState(0);
-  const [orderId, setOrderId] = useState('');
-  const [orderInfo, setOrderInfo] = useRecoilState(orderInfoState);
-
+function OrderSheetBox({order, orderInfo, setOrderInfo, progress}) {
   const onChangeOrderQuantity = (e, orderListId) => {
     const num = e.target.value;
     const orderList = orderInfo.orderList.filter(order => order.orderListId === orderListId)[0];
     const initAmount = orderList.amount / orderList.quantity;
     const newOrderList = {...orderList, quantity: num, amount: initAmount * num}
     const orderListExcept = orderInfo.orderList.filter(order => order.orderListId !== orderListId);
-
+    
     setOrderInfo((prev) => ({
       ...prev, 
       finalAmount: prev.finalAmount - orderList.amount + initAmount * num,
       orderList: [...orderListExcept, newOrderList]
     }))
   }
+
+  const onClickDeleteBtn = (orderListId) => {
+    const orderList = orderInfo.orderList.filter(order => order.orderListId === orderListId)[0];
+    const orderListExcept = orderInfo.orderList.filter(order => order.orderListId !== orderListId);
+    setOrderInfo((prev) => ({
+      ...prev, 
+      finalAmount: prev.finalAmount - orderList.amount,
+      orderList: [...orderListExcept]
+    }))
+  }
+
+  return (
+    <div className="flex">
+      <span>메뉴: {order.menu}</span>
+      <span className="ml-2">스타일: {order.style}</span>
+      <span className="ml-2">가격: {order.amount}</span>
+      <span className="ml-2">주문 수량: </span>
+      <input type="number" min="1" value={order.quantity} onChange={e => onChangeOrderQuantity(e, order.orderListId)} className="text-center w-10 outline-none" />
+      {progress === 0 && <button onClick={() => onClickDeleteBtn(order.orderListId)} className="px-2 font-bold">X</button>}
+    </div>
+  )
+}
+
+function Order({ isLogin, uid }) {
+  const [progress, setProgress] = useState(0);
+  const [orderId, setOrderId] = useState('');
+  const [orderInfo, setOrderInfo] = useRecoilState(orderInfoState);
 
   return (
     <div className="h-2/3 w-2/3 mx-auto my-0 flex flex-col items-center justify-center">
@@ -59,15 +82,12 @@ function Order({ isLogin, uid }) {
       </div>
       <ProgressBar progress={progress} />
       {progressChanger(progress, setProgress, orderId, setOrderId, isLogin, uid)}
-      <h2>주문 내역</h2>
-      {orderInfo.orderList.map(order => (
-        <div key={order.orderListId}>
-          <span>메뉴: {order.menu}</span>
-          <span>스타일: {order.style}</span>
-          <span>가격: {order.amount}</span>
-          <span>주문 수량: </span>
-          <input type="number" min="1" value={order.quantity} onChange={e => onChangeOrderQuantity(e, order.orderListId)} className="text-center w-10 outline-none" />
-        </div>
+      <h2 className="font-bold text-lg">주문서</h2>
+      {orderInfo.orderList
+        .filter((list) => list.orderListId > 0)
+        .sort((a, b) => a.orderListId - b.orderListId)
+        .map(order => (
+        <OrderSheetBox key={order.orderListId} order={order} orderInfo={orderInfo} setOrderInfo={setOrderInfo} progress={progress}/>
       ))}
       <div className="text-lg font-bold">최종 결제 금액: ${orderInfo.finalAmount}</div>
     </div>
